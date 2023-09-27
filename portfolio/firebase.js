a539ba0dfba8dd03c4091df545baa6fec98144f2
SnehalMarutiSalokhe <snehal.maruti.salokhe@gmail.com>
2023-09-27 22:15:47 +0530

    some changes related data base

---
+++ portfolio/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBFHcYpeOLmbjgzZccKcLANde_WHqdxxxQ",
+  authDomain: "portfolio-a58d9.firebaseapp.com",
+  projectId: "portfolio-a58d9",
+  storageBucket: "portfolio-a58d9.appspot.com",
+  messagingSenderId: "48826078440",
+  appId: "1:48826078440:web:c11cc67f2d772e37f3255d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
---
+++ portfolio/src/Components/contact/Contact.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import { useState } from "react";
+import { Container } from "react-bootstrap";
+import ContactImg from "../../assets/img/contact-img.svg";
+export const contact = () => {
+    const formInitialDetails={
+        firstName:'',
+        lastName:'',
+        email:'',
+        phone:'',
+        message:'',
+    }
+    const [formDtails, setformDtails] = useState(formInitialDetails);
+    const [buttonText, setButtonText] = useState('send')  ;
+    const [setstatus, setSetstatus] = useState({});
+
+    const onFormUpdate =(category , value) =>{
+        setformDtails({
+            ...formDtails,
+            [category]:value
+        })
+    }
+  return (
+    <div>
+      <section className="contact" id="connect">
+        <Container >
+            <Row className="align-items-center">
+                <Col md ={6}>
+                    <img src={ContactImg} alt="Contact Us" />
+                </Col>
+                <Col md ={6}>
+                    <h2>Get In Touch</h2>
+                    <form action="">
+                        <Row>
+                            <Col sm={6} ClassName="px-1">
+                                <input type="text" vlaue={formDtails.firstName} placeholder="First Name" onChange= {(e) =>onFormUpdate('firstName',e.target.value)} />
+                            </Col>
+                            <Col sm={6} ClassName="px-1">
+                                <input type="text" vlaue={formDtails.lasttName} placeholder="Last Name" onChange= {(e) =>onFormUpdate('lastName',e.target.value)} />
+                            </Col>
+                            <Col sm={6} ClassName="px-1">
+                                <input type="email" vlaue={formDtails.email} placeholder="email" onChange= {(e) =>onFormUpdate('email',e.target.value)} />
+                            </Col>
+                            <Col sm={6} ClassName="px-1">
+                                <input type="tel" vlaue={formDtails.phone} placeholder="phone NO" onChange= {(e) =>onFormUpdate('phone',e.target.value)} />
+                            </Col>
+                            <Col sm={6} ClassName="px-1">
+                                <textarea  row="6" vlaue={formDtails.message} placeholder="Message" onChange= {(e) =>onFormUpdate('message',e.target.value)}> < textarea/>
+                                <button type="submit"> <span> {buttonText}</span> </button>
+                            </Col>
+                        </Row>
+                    </form>
+                </Col>
+            </Row>
+        </Container>
+
+      </section>
+    </div>
+  )
+}
